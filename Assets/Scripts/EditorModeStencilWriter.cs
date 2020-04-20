267d281825f303400ee5f704e45544d69d2ea9f4
tai <taikitpmiita@icloud.com>
2020-04-20 13:49:18 +0900

    メイン　追加

---
+++ Assets/Scripts/EditorModeStencilWriter.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteAlways]
+[DefaultExecutionOrder(1)]
+public class EditorModeStencilWriter : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private Shader maskShader;
+    private static Material maskMaterial;
+    private static CommandBuffer commands;
+    private static Camera[] cameras;
+
+    private void Awake()
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (maskMaterial == null)
+        {
+            if (this.maskShader == null)
+            {
+                this.maskShader = Shader.Find("Hidden/CarverMask");
+            }
+
+            if (this.maskShader != null)
+            {
+                maskMaterial = new Material(this.maskShader);
+            }
+        }
+
+        if (commands != null)
+        {
+            return;
+        }
+
+        commands = new CommandBuffer {name = "Fill Stencil Buffer"};
+        commands.Blit(EditorGUIUtility.whiteTexture, BuiltinRenderTextureType.CameraTarget, maskMaterial);
+        cameras = SceneView.GetAllSceneCameras().Concat(Camera.allCameras).Select(
+            cam =>
+            {
+                cam.AddCommandBuffer(CameraEvent.BeforeForwardOpaque, commands);
+                return cam;
+            }).ToArray();
+    }
+
+    private void OnDisable()
+    {
+        if (cameras == null)
+        {
+            return;
+        }
+
+        foreach (var cam in cameras)
+        {
+            if (cam == null)
+            {
+                continue;
+            }
+
+            cam.RemoveCommandBuffer(CameraEvent.BeforeForwardOpaque, commands);
+        }
+
+        cameras = null;
+        commands = null;
+    }
+
+#else
+    private void Awake()
+    {
+        Destroy(this);
+    }
+#endif
+}
---
+++ Assets/Shaders/HiddenCarverMask.shader
@@ -0,0 +1,38 @@
+﻿Shader "Hidden/CarverMask"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Pass
+        {
+            ColorMask 0
+            Cull Back
+            ZTest Always
+            ZWrite Off
+            Stencil
+            {
+                Ref 1
+                WriteMask 1
+                Pass IncrWrap
+            }
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            float4 vert(float3 vertex : POSITION) : SV_POSITION
+            {
+                return UnityObjectToClipPos(vertex);
+            }
+
+            fixed4 frag() : SV_Target
+            {
+                return 0;
+            }
+            ENDCG
+        }
+    }
+}
