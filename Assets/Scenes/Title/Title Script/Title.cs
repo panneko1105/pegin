318ae0faa6a56e421c6ab09e08b355540b15bf0d
miyo46 <62433486+miyo46@users.noreply.github.com>
2020-04-21 13:43:18 +0900

    Playerってところに段差するのある

--- Assets/Scenes/Title/Title Script/Title.cs
+++ Assets/Scenes/Title/Title Script/Title.cs
@@ -5,15 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Title: MonoBehaviour
 {
-    public GameObject FadePanel;//追加
-    Fade fadeSC;
+   //GameObject test;
+    Fade fade;
     bool flg = true;
     // Use this for initialization
     void Start()
     {
-        fadeSC = FadePanel.GetComponent<Fade>();
+        fade = GetComponent<Fade>();
         flg = true;
-        fadeSC.StartFadeIn(3.0f);
+        fade.StartFadeIn(3.0f);
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class Title: MonoBehaviour
             if (Input.GetMouseButton(0))
             {
                 flg = false;
-                fadeSC.StartFadeOut("main", 3.0f);
+                fade.StartFadeOut("main", 3.0f);
             }
         }   
     }
