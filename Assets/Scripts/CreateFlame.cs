77c1acfbc097dc16b5177329873295bd12deb5a7
tai <taikitpmiita@icloud.com>
2020-04-27 01:39:08 +0900

    安定

---
+++ Assets/Scripts/CreateFlame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFlame : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // CubeプレハブをGameObject型で取得
+            GameObject obj = (GameObject)Resources.Load("flame");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-0.5f, -1.0f, 1.0f), Quaternion.identity);
+        }
+    }
+
+}
--- Assets/Scripts/FlameMove.cs
+++ Assets/Scripts/FlameMove.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
 
 public class FlameMove : MonoBehaviour
 {
-    public float move = 0.05f;
+    public float move = 0.125f;
     [HideInInspector] public int flg = 0;
     [HideInInspector] public bool isRot = false;
+    private List<Vector3> v = new List<Vector3>();
+
+    PolygonCollider2D m_ObjectCollider;
+
+    private GameObject _child;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_ObjectCollider = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
+
     {
         // 上移動
         if (Input.GetKey(KeyCode.W))
@@ -76,12 +84,45 @@ public class FlameMove : MonoBehaviour
         }
 
         // Enterキーが押されたときの処理をここに書く
-        if (Input.GetKeyDown(KeyCode.Return)) {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            m_ObjectCollider.isTrigger = false;
+            this.gameObject.AddComponent<Rigidbody2D>();
+            var script = GetComponent<FlameMove>();
 
+            var targetCollider = this.gameObject.GetComponent<Collider2D>();
+
+            var overlappingColliders = new List<Collider2D>();
+            targetCollider.OverlapCollider(new ContactFilter2D(), overlappingColliders);
+            var carvers = overlappingColliders.Select(c => c.GetComponentInChildren<Carver>())
+                .Where(c => c != null);
+            var thisCarver = targetCollider.GetComponentInChildren<Carver>();
+
+            Debug.Log(
+                $"Carve {targetCollider.name} with {string.Join(", ", carvers.Select(c => c.Collider2D.name))}.");
+            Carver.Carve(thisCarver, carvers);
+
+            DrawMesh dr = this.gameObject.GetComponent<DrawMesh>();
+            MeshFilter mf = this.gameObject.GetComponent<MeshFilter>();
+            Vector3[] test = mf.mesh.vertices;
+
+           
+            foreach (Vector3 item in test)
+            {
+                v.Add(item);
+                Debug.Log(item);
+            }
+            dr.CreateMesh(v);
+
+            var ChangeMaterial = transform.GetChild(0).gameObject.GetComponent<Carver>();
+
+            this.GetComponent<Renderer>().sharedMaterial = ChangeMaterial.GetMaterial();
+
+
+            ChangeMaterial.Change();
+            Destroy(script);
+           
         }
     }
-    void FixedUpdate()
-    {
-
-    }
+ 
 }
